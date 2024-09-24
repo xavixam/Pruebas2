b97e7f8b84a8124ac81832be117d9e050924fa79
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-09-24 13:54:48 +0200

    no message

---
+++ ejercicios.js
@@ -0,0 +1,38 @@
+//1 
+function mayusc (nombre) {
+
+    for (let i = 0; i < nombre.length; i++) {
+
+        if(nombre.charAt(i) == "a" || 
+        nombre.charAt(i) == "e" || 
+        nombre.charAt(i) == "i" || 
+        nombre.charAt(i) == "o" || 
+        nombre.charAt(i) == "u") {
+
+            i = i.toUpperCase();
+            console.log(i);
+
+        } else {
+
+            i = i.toLowerCase();
+            console.log(i);
+
+        }
+        
+
+    }
+
+}
+
+mayusc("Xavier")
+
+//2
+
+
+//3
+
+
+//5
+
+
+//6 
